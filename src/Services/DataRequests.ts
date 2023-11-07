081c93ce3d99dcdbab6a8d6fe54da9eb767291ec
Mai Mostafa <mayamostafa@gmail.com>
2023-11-07 08:27:47 -0500

    adding all functions

---
+++ src/Services/DataRequests.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { spfi, SPFx } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/views";
+import "@pnp/sp/items/get-all";
+import {SPHttpClient, ISPHttpClientOptions} from "@microsoft/sp-http";
+ 
+
+export const getAllViews = async (context: WebPartContext, siteUrl: string, listName: string) : Promise <any> => {
+  console.log("Function getAllViews --- ");
+  const sp = spfi(siteUrl).using(SPFx(context));  
+  const listViews = await sp.web.lists.getByTitle(listName).views();
+  return listViews;
+};
+
+
+export const getLargeListItems = async (context: WebPartContext, siteUrl: string, listName: string, viewName: string,  numItems: string) : Promise <any> => {
+ 
+  const sp = spfi(siteUrl).using(SPFx(context));  
+
+  const listView = await sp.web.lists.getByTitle(listName).views.getByTitle(viewName).select("ViewQuery")();
+  const xml = `<View><Query>${listView.ViewQuery}</Query><RowLimit>${numItems}</RowLimit></View>`;
+  const items = await sp.web.lists.getByTitle(listName).getItemsByCAMLQuery({ViewXml : xml});
+
+  console.log("items", items);
+
+  return items.map((item: any)=> {
+      return {
+          id: item.Id,
+          title: item.Title,
+          created: item.Created,
+          status: item.Title, // item.Status,
+          empName: item.field_1, // item.Employee_x0020_Name,
+          empNum: item.field_2, // item.Employee_x0020_Num,
+          startDate: item.field_3, // item.StartDate,
+          endDate: item.field_4, // item.End_x0020_Date,
+          totalCost: item.field_5, // item.Total_x0020_Cost,
+          approver: item.field_6, // item.Approver,
+          totalKM: item.field_7, // item.Total_x0020_KM
+      }
+  });
+
+};
+
+/*export const isObjectEmpty = (items: any): boolean=>{
+  let isEmpty:boolean = true;
+  for (const item in items){
+    isEmpty = items[item] === "" && isEmpty ;
+  }
+  return isEmpty;
+};*/
+
+
+
+
+
+export const updateListItems = async(context: WebPartContext, siteUrl: string,  listTitle:string, listItems: any, status: string) =>{
+  for(const listItem of listItems){
+      const restUrl = `${siteUrl}/_api/web/lists/getByTitle('${listTitle}')/items(${listItem.id})`;
+      // const body = JSON.stringify({Status: status});
+      const body = JSON.stringify({Title: status});
+
+      const spOptions: ISPHttpClientOptions = {
+          headers:{
+              Accept: "application/json;odata=nometadata", 
+              "Content-Type": "application/json;odata=nometadata",
+              "odata-version": "",
+              "IF-MATCH": "*",
+              "X-HTTP-Method": "MERGE",    
+          },
+          body: body
+      };
+      const _data = await context.spHttpClient.post(restUrl, SPHttpClient.configurations.v1, spOptions);
+      
+      if (_data.ok){
+          console.log('Item(s) is/are updated!');
+          return 1;
+      }
+  }
+};
+
+
+export const updateListItem = async(context: WebPartContext, siteUrl: string,  listTitle:string, listItem: any, status: string) =>{
+  const restUrl = `${siteUrl}/_api/web/lists/getByTitle('${listTitle}')/items(${listItem.id})`;
+  // const body = JSON.stringify({Status: status});
+  const body = JSON.stringify({Title: status});
+
+  const spOptions: ISPHttpClientOptions = {
+      headers:{
+          Accept: "application/json;odata=nometadata", 
+          "Content-Type": "application/json;odata=nometadata",
+          "odata-version": "",
+          "IF-MATCH": "*",
+          "X-HTTP-Method": "MERGE",    
+      },
+      body: body
+  };
+  const _data = await context.spHttpClient.post(restUrl, SPHttpClient.configurations.v1, spOptions);
+  
+  if (_data.ok){
+      console.log('Item(s) is/are updated!');
+      return 1;
+  }
+};
--- src/webparts/mrfActions/components/MrfActions.tsx
+++ src/webparts/mrfActions/components/MrfActions.tsx
@@ -1,43 +1,231 @@
 import * as React from 'react';
 import styles from './MrfActions.module.scss';
 import { IMrfActionsProps } from './IMrfActionsProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import {getLargeListItems, updateListItem, getAllViews} from '../../../Services/DataRequests';
+import { ListView, SelectionMode} from "@pnp/spfx-controls-react/lib/ListView";
+import { Stack, DefaultButton } from '@fluentui/react';
 
-export default class MrfActions extends React.Component<IMrfActionsProps, {}> {
-  public render(): React.ReactElement<IMrfActionsProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+export default function MrfActions(props:IMrfActionsProps){
 
-    return (
-      <section className={`${styles.mrfActions} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+  const {
+    hasTeamsContext,
+    context
+  } = props;
+
+
+  const [listItems, setListItems] = React.useState([]);
+  const [selItems, setSelItems] = React.useState([]);
+  const [numItemsUpdated, setNumItemsUpdated] = React.useState(0);
+  const [progressVis, setProgressVis] = React.useState(false);
+
+  React.useEffect(()=>{
+    console.log("React useEffect!");
+    getLargeListItems(context, props.siteUrl, props.listName, props.viewName, props.numItems).then(res => {
+      console.log("all items", res);
+      setListItems(res);
+    });
+
+    getAllViews(context, props.siteUrl, props.listName).then(res => console.log("alll views of list " + props.listName, res));
+
+  }, []);
+
+  const _getSelection = (items: any[]) =>{
+    console.log("selected items", items);
+    setSelItems(items);
+  };
+
+  const updateItemsStatus = (status: string) => {
+    
+    /*updateListItems(context, props.siteUrl, props.listName, selItems, status).then(()=>{
+      const selIds = selItems.map(item => item.id);
+      const newListItems = listItems.map(item => {
+        if (selIds.indexOf(item.id) !== -1){
+          return {...item, status:status}
+        }
+        return item;
+      });
+      setListItems(newListItems);
+    });*/
+
+    setProgressVis(true);
+    const bulkUpdate = async () => {
+      const updateResponseArr = [];
+      for(const selItem of selItems){
+        const updateResponse = await updateListItem(context, props.siteUrl, props.listName, selItem, status);
+        if (updateResponse === 1) setNumItemsUpdated(prev => prev+1);
+        updateResponseArr.push(updateResponse);
+      }
+      Promise.all(updateResponseArr).then(()=>{
+        const selIds = selItems.map(item => item.id);
+        const newListItems = listItems.map(item => {
+          if (selIds.indexOf(item.id) !== -1){
+            return {...item, status:status}
+          }
+          return item;
+        });
+        setListItems(newListItems);
+      });
+    };
+    bulkUpdate().then(()=>{
+      setProgressVis(false);
+      setNumItemsUpdated(0);
+    });
+
+    
+    /*for(const selItem of selItems){
+      updateListItem(context, props.siteUrl, props.listName, selItem, status).then(res => {
+        if (res === 1) setNumItemsUpdated(prev => prev+1);
+      });
+    }*/
+
+  };
+
+  const viewFields = [
+		{
+			name: "status",
+			displayName: "Status",
+			sorting: true,
+			minWidth: 20,
+			maxWidth: 50,
+      render: (item: any) =>{
+        switch (item.status){
+          case 'Not Started':
+            return <img width="16px" src="https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/NotStarted.png" alt='Not started' title='Not started' />;
+          case 'Completed':
+            return <img width="16px" src="https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/Completed.png" alt='Completed' title='Completed' />;
+          case 'Deferred':
+            return <img width="16px" src="https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/deferred.png" alt='Deferred' title='Deferred' />;
+          case 'In Progress':
+            return <img width="16px" src='https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/InProgress.png' alt='In progress' title='In progress' />;
+          case 'Waiting on someone else':
+            return <img width="16px" src='https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/Waiting.png' alt='Waiting on someone else' title='Waiting on someone else' />;
+          case 'Exported':
+            return <img width="16px" src='https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/Exported.png' alt='Exported' title='Exported' />;
+          default:
+            return <img width='16px' src='https://pdsb1.sharepoint.com/sites/My-Site/SiteAssets/icons/Status/NotStarted.png' alt='Not started' title='Not started' />
+        }
+      }
+		},
+		{
+			name: "empName",
+			displayName: "Employee Name",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 140,
+			isResizable: true,
+		},
+		{
+			name: "empNum",
+			displayName: "Employee #",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 120,
+			isResizable: true,
+		},
+		{
+			name: "startDate",
+			displayName: "Start Date",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 110,
+			isResizable: true,
+      render: (item: any) =>{
+        return new Date(item.startDate).toLocaleDateString();
+      }
+		},
+		{
+			name: "endDate",
+			displayName: "End Date",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 110,
+			isResizable: true,
+      render: (item: any) =>{
+        return new Date(item.endDate).toLocaleDateString();
+      }
+		},
+		{
+			name: "totalCost",
+			displayName: "Total Cost",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 120,
+			isResizable: true,
+      render: (item: any) => {
+        return '$' + item.totalCost ;
+      }
+		},
+		{
+			name: "approver",
+			displayName: "Approver",
+			sorting: true,
+			minWidth: 180,
+			maxWidth: 400,
+			isResizable: true,
+		},
+		{
+			name: "totalKM",
+			displayName: "Total KM",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 120,
+			isResizable: true,
+		},
+		{
+			name: "id",
+			displayName: "ID",
+			sorting: true,
+			minWidth: 100,
+			maxWidth: 100,
+			isResizable: true,
+		},
+  ];
+
+  const percenatge = numItemsUpdated * 100/selItems.length;
+
+  let totalCost = 0;
+  listItems.forEach(item => totalCost+= item.totalCost);
+
+  return (
+    <div className={`${styles.mrfActions} ${hasTeamsContext ? styles.teams : ''}`} >
+
+      {progressVis &&
+        <>
+          <div className={styles.progressBar}>
+            <div className={styles.progressBarText}>Updating Items {numItemsUpdated} of {selItems.length}</div>
+            <div className={styles.progressRate} style={{width: percenatge + '%'}} />
+          </div>
+          <div className={styles.listOverlay}/>
+        </>
+      }
+
+      <div>Select Item(s) from the below list and mark them as:</div>
+      <Stack className={styles.actionBtns} horizontal wrap tokens={{childrenGap: 15}}>
+        <DefaultButton primary iconProps={{iconName: 'Completed12'}} onClick={()=>updateItemsStatus('Completed')}>Completed</DefaultButton> 
+        <DefaultButton primary iconProps={{iconName: 'ReceiptUndelivered'}} onClick={()=>updateItemsStatus('Pending')}>Pending</DefaultButton> 
+        <DefaultButton primary iconProps={{iconName: 'Clock'}} onClick={()=>updateItemsStatus('Deferred')}>Deferred</DefaultButton>        
+      </Stack>
+
+      <div className={styles.listCntnr}>
+        <div className={styles.selectedItemsCount}>Selected Items: {selItems.length}</div>
+        <ListView
+          items={listItems}
+          viewFields={viewFields}
+          compact={true}
+          selectionMode={SelectionMode.multiple}
+          selection={_getSelection}
+          showFilter={true}
+          defaultFilter=""
+          filterPlaceHolder="Search..."
+          dragDropFiles={true}
+          stickyHeader={true}
+          className={styles.listView}
+        />
+        <div className={styles.itemsCount}>Count: {listItems.length}</div>
+        <div className={styles.totalCost}><b>Total Cost: </b>${totalCost.toLocaleString()}</div>
+      </div>
+    </div>
+  )
 }
+
+
